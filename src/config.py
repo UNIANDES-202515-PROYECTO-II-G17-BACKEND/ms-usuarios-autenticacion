d80ba76fb5931cd455d15059764b78fcca989ad1
Angel Racini <a.racini@uniandes.edu.co>
2025-10-10 19:56:37 -0500

    FIX: VARIABLES DE AMBIENTE

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ class Settings:
     REGION = os.getenv("REGION", "us-central1")
 
     DB_USER = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+    DB_PASSWORD = os.getenv("DB_PASS", "postgres")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = int(os.getenv("DB_PORT", "5432"))
     DB_NAME = os.getenv("DB_NAME", "db_ms_usuarios_aut")
